0dac983b9161f5499ddcb314249d32c8dc1494a6
eyupGuc <meydanguresi@gmail.com>
2022-10-28 20:50:05 +0300

    m

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,22 +1,10 @@
 import { useState } from "react";
 
 const Login = () => {
-    const[formValues,setFormValues]=useState({
-        username:"" , password:""
-    })
-
-   const{username,password}=formValues; 
- 
-
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-    console.log("Submitted Clicked");
-}
-
   return (
     <div className="container border border-3 mt-5 w-75 bg-info">
       <h1 className="text-center mt-4">LOGİN</h1>
-      <form onSubmit={handleSubmit}>
+      <form>
         <div className="mb-3">
           <label htmlFor="username" className="form-label">
             Username
@@ -28,8 +16,6 @@ const Login = () => {
             id="username"
             placeholder="Enter your username"
             required
-            value={username}
-         
           />
         </div>
 
@@ -44,17 +30,20 @@ const Login = () => {
             id="password"
             placeholder="Enter your password"
             required
-            value={password}
-          
           />
         </div>
         <div className="text-center">
-          <button  className="btn btn-success mb-4">
+          <button
+            onClick={() => console.log("click")}
+            className="btn btn-success mb-4"
+          >
             Login
           </button>
         </div>
       </form>
+      <Home/>
     </div>
   );
 };
+
 export default Login;
